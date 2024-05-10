d1f4b91444ee0199cf795eddbc035e8d6a78b736
arch-primal <tarduvias@gmail.com>
2024-05-10 16:23:06 -0600

    [Fix] Nitcoded's sender

--- 7-Nitcoded/sender.sh
+++ 7-Nitcoded/sender.sh
@@ -9,23 +9,28 @@ fi
 index=$(cat /root/index)
 
 # Reiniciar índice si alcanza 90
-if [ "$index" -eq 90 ]; then
+if (( "$index" >= 90 )); then
   echo -n "0" > /root/index
+  echo "$(date) - Todos los archivos fueron enviados." >> /root/sender.log
+  exit 1
 fi
 
 # Enviar 10 archivos
-for i in {1..10}; do
-  curl -T "/root/files/file$(($index + $i)).txt" "http://nitcoded-server:80/file$i.txt"
+for i in {0..9}; do
+  if [[ ! -f "/root/files/file$(($index + $i)).txt" ]]; then
+    echo "$(date) - Archivo file$(($index + $i)).txt no encontrado." >> /root/sender.log
+  fi
+
+  curl -T "/root/files/file$(($index + $i)).txt" "http://localhost:80/file$i.txt" 2>/dev/null
 
   if [ $? -ne 0 ]; then
     echo "$(date) - Error al enviar archivos. Reiniciando envío." >> /root/sender.log
     echo -n "0" > /root/index
     exit 1
+  else
+    echo "$(date) - Archivo file$(($index + $i)).txt enviado correctamente." >> /root/sender.log
   fi
 done
-
-# Log de éxito
-echo "$(date) - Archivos enviados correctamente." >> /root/sender.log
 
 # Incrementar el índice en 10 y guardarlo
 index=$(($index + 10))
